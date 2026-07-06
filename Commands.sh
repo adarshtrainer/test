78253225ea6399242b58d4a2ac2e226896cb32e1
adarshtrainer <adarshaadi1996@gmail.com>
2026-07-06 11:36:22 +0530

    New file creation

---
+++ Commands.sh
@@ -0,0 +1,11 @@
+git init => It will create new empty Git repository in the current directory.
+git add . => It will add all the files in the current directory to the staging area.
+git commit -m " commit message" => It will commit the changes in the staging area with the specified message.
+git branch -M main => It will rename the current branch to "main".
+git remote add origin <remote repository URL> => It will add a remote repository with the specified 
+git push -u origin main => It will push the committed changes to the remote repository on the "main" branch.
+
+
+git add .
+git commit -m "commit message"
+git push => It will push the committed changes to the remote repository on the current branch.
